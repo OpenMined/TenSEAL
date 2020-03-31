55b5f0b508545d8f807354b1d4a4df5149422512
youben11 <ayouben9@gmail.com>
2020-03-31 17:16:38 +0100

    CKKS python API + testing encryption/decryption

--- tenseal/tensors/__init__.py
+++ tenseal/tensors/__init__.py
@@ -23,4 +23,22 @@ def bfv_naive_vector(context, public_key, plaintext_vector):
     return _ts_cpp.BFVNaiveVector(context, public_key, plaintext_vector)
 
 
-__all__ = ["bfv_naive_vector"]
+def ckks_vector(context, public_key, scale, plaintext_vector):
+    """Constructor method for the CKKSVector object, which can store a list
+    of integers in encrypted form, using the CKKS homomorphic encryption
+    scheme.
+
+    Args:
+        context: a SEALContext object, holding the encryption parameters.
+        public_key: a PublicKey object to be used for encrypting the vector.
+        scale: the scale to be used to encode vector values.
+        plaintext_vector: a list of float to be encrypted.
+
+    Returns:
+        CKKSVector object.
+    """
+
+    return _ts_cpp.CKKSVector(context, public_key, scale, plaintext_vector)
+
+
+__all__ = ["bfv_naive_vector", "ckks_vector"]
--- tests/test_enc_dec.py
+++ tests/test_enc_dec.py
@@ -1,6 +1,17 @@
 import tenseal as ts
 
 
+def _almost_equal(vec1, vec2, m_pow_ten):
+    if len(vec1) != len(vec2):
+        return False
+
+    upper_bound = pow(10, - m_pow_ten)
+    for v1, v2 in zip(vec1, vec2):
+        if abs(v1 - v2) > upper_bound:
+            return False
+    return True
+
+
 def test_bfv_naive_encryption_decryption():
     parms = ts.bfv_parameters(4096, 1024)
     context = ts.context(parms)
@@ -12,3 +23,16 @@ def test_bfv_naive_encryption_decryption():
     decrypted_vec = bfv_vec.decrypt(sk)
 
     assert decrypted_vec == plain_vec, "Decryption of vector is incorrect."
+
+
+def test_ckks_encryption_decryption():
+    parms = ts.ckks_parameters(8192, [60, 40, 40, 60])
+    context = ts.context(parms)
+    keygen = ts.key_generator(context)
+    pk, sk = keygen.public_key(), keygen.secret_key()
+    scale = pow(2, 40)
+
+    plain_vec = [73, 81, 90]
+    ckks_vec = ts.ckks_vector(context, pk, scale, plain_vec)
+    decrypted_vec = ckks_vec.decrypt(sk)
+    assert _almost_equal(decrypted_vec, plain_vec, 1), "Decryption of vector is incorrect"
