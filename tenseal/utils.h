873207b749b1d004928434f9f7b83325558155ee
youben11 <ayouben9@gmail.com>
2020-05-11 19:14:12 +0100

    utility functions

--- tenseal/utils.h
+++ tenseal/utils.h
@@ -1,5 +1,5 @@
 #ifndef TENSEAL_UTILS_H
-#define TESNEAL_UTILS_H
+#define TENSEAL_UTILS_H
 
 #include <seal/seal.h>
 
@@ -32,6 +32,47 @@ Returns a smart pointer to a SEALContext created with the provided encryption
 parameters.
 */
 shared_ptr<seal::SEALContext> create_context(EncryptionParameters parms);
+
+/*
+Replicate the current vector as many times to fill `final_size` elements.
+*/
+template <typename T>
+void replicate_vector(vector<T>& vec, size_t final_size) {
+    size_t init_size = vec.size();
+    vec.reserve(final_size);
+    for (size_t i = 0; i < final_size - init_size; i++) {
+        vec.push_back(vec[i % init_size]);
+    }
+}
+
+/*
+Returns the k-th diagonal of a matrix. Positive values of k represent upper
+diagonals while negative values represent lower diagonal with 0 being the main
+diagonal. It's important to note that diagonals here are of the size of the
+matrix, we rotate as we reach the boundaries of the matrix.
+*/
+template <typename T>
+vector<T> get_diagonal(const vector<vector<T>> matrix, int k) {
+    size_t n_rows = matrix.size();
+    size_t n_cols = matrix[0].size();
+
+    vector<double> t_diag;
+    t_diag.reserve(n_rows * n_cols);
+
+    size_t r_offset = 0, c_offset = 0;
+    if (k > 0) {  // upper diagonals
+        c_offset = k;
+    } else {  // lower diagonal
+        r_offset = -k;
+    }
+
+    for (size_t i = 0; i < n_rows * n_cols; i++) {
+        t_diag.push_back(
+            matrix[(r_offset + i) % n_rows][(c_offset + i) % n_cols]);
+    }
+
+    return t_diag;
+}
 
 }  // namespace tenseal
 
