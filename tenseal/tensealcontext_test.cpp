12c819e0ca28d4d26b2c0cbb821ed3c659385101
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-06-04 13:16:19 +0300

    sample gtest integration

---
+++ tenseal/tensealcontext_test.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+
+namespace {
+
+class TensealContextTest : public ::testing::Test {
+   protected:
+    void SetUp() {}
+};
+
+TEST_F(TensealContextTest, TestSanity) {}
+
+}  // namespace
