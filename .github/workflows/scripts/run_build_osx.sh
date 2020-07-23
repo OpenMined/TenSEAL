89bbc7a8173464ad340afb7d7df3bccc58a1b3ed
ramesht007 <shre611shukla@gmail.com>
2020-07-23 19:53:32 +0530

    # wheel

--- .github/workflows/scripts/run_build_osx.sh
+++ .github/workflows/scripts/run_build_osx.sh
@@ -2,4 +2,5 @@
 set -e
 
 python -m pip install --upgrade pip
+pip install setuptools wheel twine auditwheel
 pip install -r requirements_dev.txt
--- .github/workflows/scripts/run_build_ubuntu.sh
+++ .github/workflows/scripts/run_build_ubuntu.sh
@@ -4,4 +4,5 @@ set -e
 sudo apt update
 sudo apt install cmake clang libprotobuf-dev protobuf-compiler
 python -m pip install --upgrade pip
+pip install setuptools wheel twine auditwheel
 pip install -r requirements_dev.txt
