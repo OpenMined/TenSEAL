150122c6d33214e4833c4662e0cf12f3fe276325
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-07-23 17:43:17 +0300

    tests

---
+++ .github/workflows/scripts/depends_macos.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+
+python -m pip install --upgrade pip
+pip install -r requirements_dev.txt
---
+++ .github/workflows/scripts/depends_ubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+
+sudo apt update
+sudo apt install cmake clang libprotobuf-dev protobuf-compiler
+python -m pip install --upgrade pip
+pip install -r requirements_dev.txt
