7f7060985b6d6ed442790b45693cb52d030982d7
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-07-23 20:12:05 +0300

    debug

--- .github/workflows/scripts/depends_macos.sh
+++ .github/workflows/scripts/depends_macos.sh
@@ -3,12 +3,5 @@ set -e
 
 brew install protobuf@3.12
 
-cd /usr/local/Cellar/protobuf/3.12.3
-
-sudo mkdir -p /usr/local/include/google/protobuf/
-
-sudo rm -r /usr/local/include/google/protobuf/
-sudo cp -r include/google/protobuf/ /usr/local/include/google/protobuf/
-
 python -m pip install --upgrade pip
 pip install -r requirements_dev.txt
