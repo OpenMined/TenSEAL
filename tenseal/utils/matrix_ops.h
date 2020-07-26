f647544cbe126615e059b7ef12c90cfb5f7190b6
youben11 <ayouben9@gmail.com>
2020-07-26 11:47:52 +0100

    lint

--- tenseal/utils/matrix_ops.h
+++ tenseal/utils/matrix_ops.h
@@ -102,7 +102,8 @@ Ciphertext diagonal_ct_vector_matmul(shared_ptr<TenSEALContext> tenseal_context,
 template <typename T, class Encoder>
 Ciphertext diagonal_ct_vector_matmul_parallel(
     shared_ptr<TenSEALContext> tenseal_context, Ciphertext& vec,
-    const size_t vector_size, const vector<vector<T>>& matrix, uint n_threads = 0) {
+    const size_t vector_size, const vector<vector<T>>& matrix,
+    uint n_threads = 0) {
     // matrix is organized by rows
     // _check_matrix(matrix, this->size())
     const size_t n_rows = matrix.size();
