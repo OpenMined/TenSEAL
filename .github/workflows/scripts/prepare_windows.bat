6c4b781664bfa5d3dd41aaef04d4b1961a52d462
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-07-27 08:18:05 +0300

    debug windows

--- .github/workflows/scripts/prepare_windows.bat
+++ .github/workflows/scripts/prepare_windows.bat
@@ -4,9 +4,12 @@ echo on
 @REM Set Environment Variables
 @REM
 IF NOT DEFINED PYTHON_DIRECTORY (
-  SET PYTHON_DIRECTORY=Python
+  SET PYTHON_DIRECTORY=Python38
 )
 SET PY_EXE=C:\%PYTHON_DIRECTORY%\python.exe
 SET PIP_EXE=C:\%PYTHON_DIRECTORY%\Scripts\pip.exe
 SET PATH=%PATH%;C:\%PYTHON_DIRECTORY%
 SET VC_ExecutablePath_x64=%VC_ExecutablePath_x64_x64%
+
+
+bazel test --test_output=all --test_timeout=900 //...
