064e56af872d1320802dc6a7bc9b9e3ed883764d
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-07-27 08:07:50 +0300

    debug windows

---
+++ .github/workflows/scripts/prepare_windows.bat
@@ -0,0 +1,12 @@
+echo on
+
+@REM
+@REM Set Environment Variables
+@REM
+IF NOT DEFINED PYTHON_DIRECTORY (
+  SET PYTHON_DIRECTORY=Python
+)
+SET PY_EXE=C:\%PYTHON_DIRECTORY%\python.exe
+SET PIP_EXE=C:\%PYTHON_DIRECTORY%\Scripts\pip.exe
+SET PATH=%PATH%;C:\%PYTHON_DIRECTORY%
+SET VC_ExecutablePath_x64=%VC_ExecutablePath_x64_x64%
