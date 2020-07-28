8ade0d3827b223170ac797472fee44f307d05559
philomath213 <bilalphilomath@gmail.com>
2020-07-28 19:55:43 +0100

    fix kernel padding bug

--- tenseal/cpp/tensors/ckksvector.cpp
+++ tenseal/cpp/tensors/ckksvector.cpp
@@ -584,15 +584,13 @@ CKKSVector& CKKSVector::conv2d_im2col_inplace(const vector<double>& kernel,
     size_t chuncks_nb = kernel.size();
 
     // check if vector size is not a power of 2
-    if (!(this->_size && (!(this->_size & (this->_size - 1))))) {
-        throw invalid_argument("vector size should be a power of 2");
+    if (!(chuncks_nb && (!(chuncks_nb & (chuncks_nb - 1))))) {
+        throw invalid_argument("Kernel size should be a power of 2");
     }
 
-    // TODO: check matrix shape
-    // if (this->_size / windows_nb != chuncks_nb) {
-    //     throw invalid_argument("matrix shape doesn't match with vector
-    //     size");
-    // }
+    if (this->_size / windows_nb != chuncks_nb) {
+        throw invalid_argument("Matrix shape doesn't match with vector size");
+    }
 
     size_t vec_len = chunck_size * chuncks_nb;
     plain_vec.reserve(this->_size);
@@ -601,9 +599,6 @@ CKKSVector& CKKSVector::conv2d_im2col_inplace(const vector<double>& kernel,
         vector<double> tmp(chunck_size, kernel[i]);
         plain_vec.insert(plain_vec.end(), tmp.begin(), tmp.end());
     }
-
-    // pad the plain_vec
-    plain_vec.resize(this->_size, 0);
 
     // replicate the vector in order to be able to do multiple matrix
     // multiplications
