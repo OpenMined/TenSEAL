c5c149e696bf6e252ea1986bbdde7c3881d357e0
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-07-29 16:25:06 +0300

    update benchmarks

--- tests/cpp/benchmarks/benchmark.cpp
+++ tests/cpp/benchmarks/benchmark.cpp
@@ -12,7 +12,7 @@ void BM_matmul_plain(benchmark::State& state) {
     ctx->global_scale(std::pow(2, 40));
 
   std::vector<double> data;
-  size_t N = 50;
+  size_t N = 64;
 
   for(size_t idx = 0; idx < N; ++idx) {
     data.push_back(idx + 1);
