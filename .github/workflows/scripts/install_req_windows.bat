eae49038b888a6d44e11084f58f26a84881d08a5
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-08-04 15:21:17 +0300

    debug windows

--- .github/workflows/scripts/install_req_windows.bat
+++ .github/workflows/scripts/install_req_windows.bat
@@ -6,7 +6,7 @@ cd vcpkg
 ./vcpkg integrate install
 
 vcpkg install protobuf
-vcpkg install protobuf:x64-windows
+vcpkg install protobuf:x64-windows-static
 
 python -m pip install --upgrade pip
-pip install -r requirements_dev.txt
+pip install -r ./requirements_dev.txt
