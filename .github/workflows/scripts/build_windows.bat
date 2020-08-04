07e82ba9b196eb44e8e2a9a598724f2d00112987
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-08-04 12:08:49 +0300

    typo

--- .github/workflows/scripts/build_windows.bat
+++ .github/workflows/scripts/build_windows.bat
@@ -1,8 +1,7 @@
 echo on
 
- git submodule init && git submodule update
+git submodule init && git submodule update
  
- msbuild .\third_party\SEAL\native\src\SEAL.vcxproj /p:configuration=Release /p:platform=x64 /p:SolutionDir=.. \..\
+msbuild .\third_party\SEAL\native\src\SEAL.vcxproj /p:configuration=Release /p:platform=x64 /p:SolutionDir=..\..\
 
- pip install .
-
+pip install .
