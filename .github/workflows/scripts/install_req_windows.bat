54baa9a329951b42f09d6186fe83570d859b8515
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-08-04 15:01:55 +0300

    debug windows

--- .github/workflows/scripts/install_req_windows.bat
+++ .github/workflows/scripts/install_req_windows.bat
@@ -1,14 +1,12 @@
 echo on
 
-
 git clone https://github.com/Microsoft/vcpkg.git
-.\vcpkg\bootstrap-vcpkg.bat
-setx VCPKG "%cd%\vcpkg" /M
+cd vcpkg
+./bootstrap-vcpkg.sh
+./vcpkg integrate install
 
-.\vcpkg\vcpkg update
-.\vcpkg\vcpkg install protobuf protobuf:x64-windows
-.\vcpkg\vcpkg integrate install
-
+vcpkg install protobuf
+vcpkg install protobuf:x64-windows
 
 python -m pip install --upgrade pip
-pip install setuptools wheel twine auditwheel
+pip install -r requirements_dev.txt
