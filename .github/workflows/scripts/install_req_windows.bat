3ca64a8da2b8cc4a450080eaac4962999443a97e
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-08-04 13:13:32 +0300

    debug windows

--- .github/workflows/scripts/install_req_windows.bat
+++ .github/workflows/scripts/install_req_windows.bat
@@ -1,8 +1,13 @@
 echo on
 
 
-vcpkg update
-vcpkg install protobuf protobuf:x64-windows
+git clone https://github.com/Microsoft/vcpkg.git
+.\vcpkg\bootstrap-vcpkg.bat
+setx VCPKG "%cd%\vcpkg" /M
+
+.\vcpkg\vcpkg update
+.\vcpkg\vcpkg install protobuf protobuf:x64-windows
+.\vcpkg\vcpkg integrate install
 
 
 python -m pip install --upgrade pip
