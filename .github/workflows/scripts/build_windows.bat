1c443c53bf2ee2fd05917495a37c94a72e9a501f
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-08-05 13:15:30 +0300

    update windows

--- .github/workflows/scripts/build_windows.bat
+++ .github/workflows/scripts/build_windows.bat
@@ -1,4 +1,4 @@
-echo on
+@echo off
 
 set PATH=%PATH%;C:/Program Files (x86)/protobuf/bin/;C:/Program Files/protobuf/bin/
 
@@ -7,7 +7,7 @@ git submodule init && git submodule update
 msbuild .\third_party\SEAL\native\src\SEAL.vcxproj /p:configuration=Release /p:platform=x64 /p:SolutionDir=..\..\
 
 mkdir proto_out
-protoc --proto_path=tenseal\proto --cpp_out=proto_out tenseal\proto\tensealcontext.proto
-protoc --proto_path=tenseal\proto --cpp_out=proto_out tenseal\proto\tensors.proto
+protoc --proto_path=tenseal\proto --cpp_out=tenseal\proto tenseal\proto\tensealcontext.proto
+protoc --proto_path=tenseal\proto --cpp_out=tenseal\proto tenseal\proto\tensors.proto
 
 pip install .
