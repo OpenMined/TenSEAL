a6f56559634dd1812af81ed8284c068005249f28
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-08-05 12:30:50 +0300

    debug windows

--- .github/workflows/scripts/build_windows.bat
+++ .github/workflows/scripts/build_windows.bat
@@ -4,4 +4,8 @@ git submodule init && git submodule update
  
 msbuild .\third_party\SEAL\native\src\SEAL.vcxproj /p:configuration=Release /p:platform=x64 /p:SolutionDir=..\..\
 
+mkdir proto_out
+protoc --proto_path=tenseal\proto --cpp_out=proto_out tenseal\proto\tensealcontext.proto
+protoc --proto_path=tenseal\proto --cpp_out=proto_out tenseal\proto\tensors.proto
+
 pip install .
