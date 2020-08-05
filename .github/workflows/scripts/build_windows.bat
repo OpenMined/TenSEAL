da86f52e374b7bc25341a13ac751c59de217c002
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-08-06 00:15:51 +0300

    cleanup

--- .github/workflows/scripts/build_windows.bat
+++ .github/workflows/scripts/build_windows.bat
@@ -1,13 +1,12 @@
 @echo off
 
-set PATH=%PATH%;C:/Program Files (x86)/protobuf/bin/;C:/Program Files/protobuf/bin/
+set PATH=%PATH%;%programfiles%/protobuf/bin/;%programfiles(x86)%/protobuf/bin/
 set _CL_=/MT
 
 git submodule init && git submodule update
  
 msbuild .\third_party\SEAL\native\src\SEAL.vcxproj /p:configuration=Release /p:platform=x64 /p:SolutionDir=..\..\
 
-mkdir proto_out
 protoc --proto_path=tenseal\proto --cpp_out=tenseal\proto tenseal\proto\tensealcontext.proto
 protoc --proto_path=tenseal\proto --cpp_out=tenseal\proto tenseal\proto\tensors.proto
 
