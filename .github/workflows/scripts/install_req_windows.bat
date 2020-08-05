0dab96b3f57e1bb1037e623c4be50bea91ee58bf
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-08-05 13:03:12 +0300

    update bat script

--- .github/workflows/scripts/install_req_windows.bat
+++ .github/workflows/scripts/install_req_windows.bat
@@ -1,7 +1,7 @@
 echo on
 
 python -m pip install --upgrade pip
-pip install -r ./requirements_dev.txt
+pip install -r ./requirements_dev.txt || goto :error
 
 git clone https://github.com/google/protobuf.git
 cd protobuf
@@ -10,6 +10,12 @@ git submodule init && git submodule update
 
 mkdir tmp && cd tmp
 
-cmake ..\cmake
-cmake --build .
-cmake --build . --target install
+cmake ..\cmake || goto :error
+cmake --build . || goto :error
+cmake --build . --target install || goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
