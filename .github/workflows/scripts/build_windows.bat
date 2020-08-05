a6bc6141c45c74603fb55713e8a5d95bbff96e2a
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-08-05 11:30:20 +0300

    debug

--- .github/workflows/scripts/build_windows.bat
+++ .github/workflows/scripts/build_windows.bat
@@ -4,6 +4,4 @@ git submodule init && git submodule update
  
 msbuild .\third_party\SEAL\native\src\SEAL.vcxproj /p:configuration=Release /p:platform=x64 /p:SolutionDir=..\..\
 
-cd tenseal\proto && cmake . && make && cd ..\..\
-
 pip install .
