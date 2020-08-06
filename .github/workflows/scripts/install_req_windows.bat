e3d52ecf3826f74903f4ed9ff79644e55c455eae
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-08-06 11:19:55 +0300

    update release yamls

--- .github/workflows/scripts/install_req_windows.bat
+++ .github/workflows/scripts/install_req_windows.bat
@@ -2,6 +2,7 @@ echo on
 
 python -m pip install --upgrade pip
 pip install -r ./requirements_dev.txt || goto :error
+pip install setuptools wheel twine auditwheel
 
 set _CL_=/MT
 
---
+++ .github/workflows/scripts/release_windows.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set PATH=%PATH%;%programfiles%/protobuf/bin/;%programfiles(x86)%/protobuf/bin/
+
+:: MT vs MD: static runtime library linking vs dynamic runtime library linking.
+:: Protubf library enforces the MT flag on the static .lib build. If we want to link it, we need to use MT everywhere, otherwise the build will fail.
+:: The general recommendation is to use MD, but for that we need to build and use protobufs as DLL.
+set _CL_=/MT
+
+git submodule init && git submodule update
+ 
+msbuild .\third_party\SEAL\native\src\SEAL.vcxproj /p:configuration=Release /p:platform=x64 /p:SolutionDir=..\..\
+
+protoc --proto_path=tenseal\proto --cpp_out=tenseal\proto tenseal\proto\tensealcontext.proto
+protoc --proto_path=tenseal\proto --cpp_out=tenseal\proto tenseal\proto\tensors.proto
+
+pip install .
