655cb71bf22bbbcd0a743d7448ef1c4a7ed7c579
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-08-06 08:09:48 +0300

    small explanation on MT vs MD

--- .github/workflows/scripts/build_windows.bat
+++ .github/workflows/scripts/build_windows.bat
@@ -1,6 +1,10 @@
 @echo off
 
 set PATH=%PATH%;%programfiles%/protobuf/bin/;%programfiles(x86)%/protobuf/bin/
+
+:: MT vs MD: static runtime library linking vs dynamic runtime library linking.
+:: Protubf library enforces the MT flag on the static .lib build. If we want to link it, we need to use MT everywhere, otherwise the build will fail.
+:: The general recommendation is to use MD, but for that we need to build and use protobufs as DLL.
 set _CL_=/MT
 
 git submodule init && git submodule update
