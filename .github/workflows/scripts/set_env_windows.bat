7733c230a8b70e4fcf6bc7127b6138979cc60358
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-08-13 22:52:50 +0300

    debug windows

--- .github/workflows/scripts/set_env_windows.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-
-set VS160COMNTOOLS="%programfiles(x86)%/Microsoft Visual Studio/2019/Enterprise/Common7/Tools/"
---
+++ .github/workflows/scripts/set_env_windows.sh
@@ -0,0 +1,6 @@
+@echo off
+
+
+export BAZEL_VS="%programfiles(x86)%/Microsoft Visual Studio/2019/BuildTools"
+export BAZEL_SH="C:/msys64/usr/bin/bash.exe"
+
