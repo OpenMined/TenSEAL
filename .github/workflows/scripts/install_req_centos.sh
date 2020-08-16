a3115ab85c3fbbbf71982c31f029e361f8a0da8c
philomath213 <bilalphilomath@gmail.com>
2020-08-16 06:11:17 +0100

    add install requirements script for centos

---
+++ .github/workflows/scripts/install_req_centos.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+# install cmake 3.17.1
+curl https://github.com/Kitware/CMake/releases/download/v3.17.1/cmake-3.17.1-Linux-x86_64.tar.gz -L | tar xz -C /opt/
+echo "::add-path::/opt/cmake-3.17.1-Linux-x86_64/bin"
+
+# install latest protobuf release
+curl https://github.com/protocolbuffers/protobuf/releases/download/v3.13.0/protobuf-cpp-3.13.0.tar.gz -L | tar xz -C /tmp
+pushd /tmp/protobuf-*/
+./configure
+make
+make install
+ldconfig
+popd
+
+# install python dependencies
+python -m pip install --upgrade pip
+pip install -r requirements_dev.txt
+pip install setuptools wheel twine auditwheel
