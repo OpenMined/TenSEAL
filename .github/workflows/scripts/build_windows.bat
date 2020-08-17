fc8bcf51a17e1901f796bcee4e1e50e40fa7cf9b
philomath213 <bilalphilomath@gmail.com>
2020-08-17 20:08:34 +0100

    update build script for windows

--- .github/workflows/scripts/build_windows.bat
+++ .github/workflows/scripts/build_windows.bat
@@ -9,7 +9,7 @@ set _CL_=/MT
 
 git submodule init && git submodule update
  
-msbuild .\third_party\SEAL\native\src\SEAL.vcxproj /p:configuration=Release /p:platform=x64 /p:SolutionDir=..\..\
+msbuild .\third_party\SEAL\native\src\SEAL.vcxproj /p:configuration=Release /p:platform=x64
 
 protoc --proto_path=tenseal\proto --cpp_out=tenseal\proto tenseal\proto\tensealcontext.proto
 protoc --proto_path=tenseal\proto --cpp_out=tenseal\proto tenseal\proto\tensors.proto
