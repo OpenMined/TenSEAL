c226f646b99bd84220c5ef94725963d7d0287de4
Bilal Retiat <bilalphilomath@gmail.com>
2020-08-19 19:12:16 +0100

    remove unnecessary check in CKKSVector encrypt method

--- .github/workflows/scripts/build_nix.sh
+++ .github/workflows/scripts/build_nix.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-# fetch third_party libraries
-git submodule init && git submodule update
 # we need to install tenseal in order to build the cpp library
 pip install .
--- .github/workflows/scripts/release_macos.sh
+++ .github/workflows/scripts/release_macos.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-# fetch third_party libraries
-git submodule init && git submodule update
 pip wheel . -w wheel/
 twine upload wheel/*
