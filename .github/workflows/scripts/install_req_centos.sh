189e567a4641285b13d87896d8409a4989803bbf
Bilal Retiat <bilalphilomath@gmail.com>
2020-08-27 22:14:25 +0100

    Fix centos script (#149)
    * fix centos script
    
    * run Linux release workflow on PR
    
    * Revert "run Linux release workflow on PR"
    
    This reverts commit 97bdd638e3a417154c9be2c3173f044f9374bf83.

--- .github/workflows/scripts/install_req_centos.sh
+++ .github/workflows/scripts/install_req_centos.sh
@@ -7,15 +7,14 @@ curl https://github.com/Kitware/CMake/releases/download/v3.17.1/cmake-3.17.1-Lin
 echo "::add-path::/opt/cmake-3.17.1-Linux-x86_64/bin"
 
 # install latest protobuf release
-mkdir /opt/protobuf/
-pushd /opt/protobuf/
-curl https://github.com/protocolbuffers/protobuf/releases/download/v3.13.0/protoc-3.13.0-linux-x86_64.zip -LO
-unzip protoc-3.13.0-linux-x86_64.zip
-rm protoc-3.13.0-linux-x86_64.zip
+curl https://github.com/protocolbuffers/protobuf/releases/download/v3.13.0/protobuf-cpp-3.13.0.tar.gz -L | tar xz -C /opt/
+pushd /opt/protobuf-3.13.0
+./configure
+make
+make install
+ldconfig
 popd
-echo "::add-path::/opt/protobuf/bin"
 
 # install python dependencies
 python -m pip install --upgrade pip
-pip install -r requirements_dev.txt
 pip install setuptools wheel twine auditwheel
