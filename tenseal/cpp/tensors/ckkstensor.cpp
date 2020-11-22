bbb93c5a5c28d6e4ec452a5f9e4b106fcd8107bd
youben11 <ayouben9@gmail.com>
2020-11-22 16:43:35 +0100

    rm encrypt with single plain value

--- tenseal/cpp/tensors/ckkstensor.cpp
+++ tenseal/cpp/tensors/ckkstensor.cpp
@@ -18,13 +18,8 @@ CKKSTensor::CKKSTensor(const shared_ptr<TenSEALContext>& ctx,
 
     vector<double> plain_data = tensor.data();
     for (int i = 0; i < plain_data.size(); i++)
-        _data.push_back(
-            CKKSTensor::encrypt(ctx, this->_init_scale, plain_data[i]));
-}
-
-Ciphertext CKKSTensor::encrypt(const shared_ptr<TenSEALContext>& ctx,
-                               const double scale, const double data) {
-    return encrypt(ctx, scale, vector<double>({data}));
+        _data.push_back(CKKSTensor::encrypt(ctx, this->_init_scale,
+                                            vector<double>({plain_data[i]})));
 }
 
 Ciphertext CKKSTensor::encrypt(const shared_ptr<TenSEALContext>& ctx,
