3596afc07bd838e0ba10c00099228ff4d166e6ac
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-11-23 12:13:16 +0200

    debug windows

--- .github/workflows/scripts/build_windows.bat
+++ .github/workflows/scripts/build_windows.bat
@@ -2,12 +2,6 @@
 
 set PATH=%PATH%;%programfiles%/protobuf/bin/;%programfiles(x86)%/protobuf/bin/
 
-cd third_party\SEAL
-cmake -S . -B build -G "Visual Studio 16 2019" -A x64
-cmake --build build --config Release
-cmake --install build --config Release
-cd ..\..
-
 :: MT vs MD: static runtime library linking vs dynamic runtime library linking.
 :: Protubf library enforces the MT flag on the static .lib build. If we want to link it, we need to use MT everywhere, otherwise the build will fail.
 :: The general recommendation is to use MD, but for that we need to build and use protobufs as DLL.
