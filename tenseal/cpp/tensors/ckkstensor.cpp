970c3e16810fb93d8d0ecf8293b6373464097ec4
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-11-24 11:31:07 +0200

    use the new tensor

--- tenseal/cpp/tensors/ckkstensor.cpp
+++ tenseal/cpp/tensors/ckkstensor.cpp
@@ -16,9 +16,9 @@ CKKSTensor::CKKSTensor(const shared_ptr<TenSEALContext>& ctx,
         this->_init_scale = ctx->global_scale();
     }
 
-    for (auto& item : tensor.data())
-        _data.push_back(CKKSTensor::encrypt(ctx, this->_init_scale,
-                                            vector<double>({item})));
+    for (auto it = tensor.cbegin(); it != tensor.cend(); it++)
+        _data.push_back(
+            CKKSTensor::encrypt(ctx, this->_init_scale, vector<double>({*it})));
 }
 
 Ciphertext CKKSTensor::encrypt(const shared_ptr<TenSEALContext>& ctx,
