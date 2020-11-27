ed9c5885ef8df8ddfd3f76a6323fed154a43be54
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-11-27 14:06:29 +0200

    update tests

--- tenseal/binding.cpp
+++ tenseal/binding.cpp
@@ -440,6 +440,10 @@ PYBIND11_MODULE(_tenseal_cpp, m) {
                          const PlainTensor<double> &tensor) {
             return CKKSTensor::Create(ctx, tensor);
         }))
+        .def(py::init(
+            [](const shared_ptr<TenSEALContext> &ctx, const std::string &data) {
+                return CKKSTensor::Create(ctx, data);
+            }))
         .def("decrypt",
              [](shared_ptr<CKKSTensor> obj) { return obj->decrypt().data(); })
         .def("decrypt",
@@ -447,7 +451,7 @@ PYBIND11_MODULE(_tenseal_cpp, m) {
                  return obj->decrypt(sk).data();
              })
         .def("context",
-             [](shared_ptr<CKKSVector> obj) { return obj->tenseal_context(); })
+             [](shared_ptr<CKKSTensor> obj) { return obj->tenseal_context(); })
         .def("serialize",
              [](shared_ptr<CKKSTensor> &obj) { return py::bytes(obj->save()); })
         .def("copy", &CKKSTensor::deepcopy)
