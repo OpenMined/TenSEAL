cb9f0a41f02008a6bab0e67e2ef082d428eba224
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-05 14:52:14 +0200

    cleanup

--- .github/workflows/scripts/install_req_docker.sh
+++ .github/workflows/scripts/install_req_docker.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-sudo apt update -y
-sudo apt install curl git build-essential cmake automake libtool libtool-bin -y
+apt update -y
+apt install curl git build-essential cmake automake libtool libtool-bin -y
 
 curl https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/protobuf-cpp-3.14.0.tar.gz -L | tar xz -C /opt/ && \
     cd /opt/protobuf-3.14.0 && \
     autoreconf --install && \
     ./configure CXXFLAGS=-fPIC && \
-    make && sudo make install && sudo ldconfig && cd -
+    make && make install && ldconfig && cd -
 
 if [ $? -ne 0 ]
 then
