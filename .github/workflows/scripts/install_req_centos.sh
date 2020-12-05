e08a3e975bb9b29fc13bd414ad77c6ddea50d9bd
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-05 17:14:13 +0200

    fix linux release script

--- .github/workflows/scripts/install_req_centos.sh
+++ .github/workflows/scripts/install_req_centos.sh
@@ -4,7 +4,7 @@ set -e
 
 # install cmake 3.17.1
 curl https://github.com/Kitware/CMake/releases/download/v3.17.1/cmake-3.17.1-Linux-x86_64.tar.gz -L | tar xz -C /opt/
-echo "::add-path::/opt/cmake-3.17.1-Linux-x86_64/bin"
+echo "/opt/cmake-3.17.1-Linux-x86_64/bin" >> $GITHUB_PATH
 
 # install latest protobuf release
 curl https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/protobuf-cpp-3.14.0.tar.gz -L | tar xz -C /opt/
