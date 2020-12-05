67e2979340394a6398a40f3a10431f16c3c1a510
Bogdan Cebere <bogdan.cebere@gmail.com>
2020-12-05 21:10:33 +0200

    fix linux release script (#181)
    * fix linux release script
    
    * enable publish
    
    * bbreak
    
    * windows bbreak
    
    * disable linux release
    
    * disable windows package on PR
    
    Co-authored-by: Ayoub Benaissa <ayouben9@gmail.com>

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -1,7 +1,4 @@
 if(WIN32)
-  set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
-  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd")
-
   include_directories(protobuf/src)
   link_libraries("$ENV{PROGRAMFILES\(x86\)}/protobuf/lib/libprotobuf-lite.lib")
   link_libraries("$ENV{PROGRAMFILES\(x86\)}/protobuf/lib/libprotobuf.lib")
