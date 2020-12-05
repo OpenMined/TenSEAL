0ccea06410bb53368061b8ee4922decef957ade2
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-05 10:58:01 +0200

    cleanup cmake

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -7,15 +7,7 @@ if(WIN32)
   link_libraries("$ENV{PROGRAMFILES\(x86\)}/protobuf/lib/libprotobuf.lib")
   link_libraries("$ENV{PROGRAMFILES\(x86\)}/protobuf/lib/libprotoc.lib")
 else()
-  # building protobuf using cmake
   add_subdirectory(tenseal/proto)
-
-  set(Protobuf_USE_STATIC_LIBS ON)
-  set(Protobuf_DEBUG ON)
-
-  find_package(Protobuf REQUIRED)
-  include(FindProtobuf)
-  include_directories(${Protobuf_INCLUDE_DIRS})
 endif()
 
 
