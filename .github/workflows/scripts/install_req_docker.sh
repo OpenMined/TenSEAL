c989d8f11d2522d6c49518a1afabde6feae9908d
Bogdan Cebere <bogdan.cebere@gmail.com>
2020-12-05 15:57:00 +0200

    Docker build (#178)
    * build static protobuf for linux
    
    * rework cmake organization
    
    * rework protobuf build
    
    * linkage issue
    
    * cleanup cmake
    
    * update release scripts
    
    * cleanup
    
    * Revert "cleanup cmake"
    
    This reverts commit 0ccea06410bb53368061b8ee4922decef957ade2.
    
    * update release script
    
    * update docker build
    
    * debug
    
    * debug
    
    * sudo install
    
    * Revert "debug"
    
    This reverts commit 0c482c9105595c6a784ddfe9d160e1d2617b06a0.
    
    * Revert "debug"
    
    This reverts commit c3b1a2370ffd3cc1a0020dc9cd159deb8c3ff496.
    
    * drop submodule
    
    * cleanup
    
    * cleanup

--- .github/workflows/scripts/install_req_docker.sh
+++ .github/workflows/scripts/install_req_docker.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-sudo apt update -y
-sudo apt install curl git build-essential cmake automake libtool libtool-bin -y
+apt update -y
+apt install curl git build-essential cmake automake libtool libtool-bin -y
 
 proto_version="3.14.0"
 curl https://github.com/protocolbuffers/protobuf/releases/download/v${proto_version}/protobuf-cpp-${proto_version}.tar.gz -L | tar xz -C /opt/ && \
     cd /opt/protobuf-${proto_version} && \
     autoreconf --install && \
     ./configure CXXFLAGS=-fPIC && \
-    make && sudo make install && sudo ldconfig && cd -
+    make && make install && ldconfig && cd -
 
 if [ $? -ne 0 ]
 then
