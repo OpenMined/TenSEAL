59d42d08cc6cc8edd5f079f5ed7d103d83a1041d
Cebere Bogdan <bogdan.cebere@gmail.com>
2021-01-16 19:01:11 +0200

    revert changes for windows

--- .github/workflows/scripts/build_windows.bat
+++ .github/workflows/scripts/build_windows.bat
@@ -1,3 +1,13 @@
 @echo off
 
+set PATH=%PATH%;%programfiles%/protobuf/bin/;%programfiles(x86)%/protobuf/bin/
+
+:: MT vs MD: static runtime library linking vs dynamic runtime library linking.
+:: Protubf library enforces the MT flag on the static .lib build. If we want to link it, we need to use MT everywhere, otherwise the build will fail.
+:: The general recommendation is to use MD, but for that we need to build and use protobufs as DLL.
+set _CL_=/MT
+
+protoc --proto_path=tenseal\proto --cpp_out=tenseal\proto tenseal\proto\tensealcontext.proto
+protoc --proto_path=tenseal\proto --cpp_out=tenseal\proto tenseal\proto\tensors.proto
+
 pip install .
