bf0b5661d047fa4a738eb52fd2b00e43a8395ac5
Cebere Bogdan <bogdan.cebere@gmail.com>
2021-01-20 15:23:20 +0200

    debug

--- .github/workflows/scripts/release_centos.sh
+++ .github/workflows/scripts/release_centos.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+cmake .
+find ./ -name protobuf-config.cmake
+
+echo "TESTING #############"
+
 pip wheel . -w dist/ --no-deps
 auditwheel repair dist/*.whl --plat $AUDITWHEEL_PLAT
 twine upload --skip-existing wheelhouse/*
