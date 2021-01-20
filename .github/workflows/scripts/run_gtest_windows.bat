222c75c7dddd3a52e80e757f1dad789c64a47009
Cebere Bogdan <bogdan.cebere@gmail.com>
2021-01-20 21:06:25 +0200

    drop gtest for windows

--- .github/workflows/scripts/run_gtest_windows.bat
+++ .github/workflows/scripts/run_gtest_windows.bat
@@ -1,9 +1,1 @@
 echo on
-
-set _CL_=/MT
-
-cmake -D BUILD_TEST=TRUE -DCMAKE_BUILD_TYPE=Release .
-cmake -D BUILD_TEST=TRUE --build .
-cmake --build . --target install --config Release
-
-.\Release\tenseal_tests.exe
