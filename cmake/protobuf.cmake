37432ddbc3b13cfc8ceeaecd4f2bd07a6c7f615e
Cebere Bogdan <bogdan.cebere@gmail.com>
2021-01-20 15:36:26 +0200

    update lib path

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -10,15 +10,18 @@ else()
 
   set(Protobuf_ROOT ${CMAKE_SOURCE_DIR}/third_party/protobuf/cmake)
   set(Protobuf_DIR
-      ${CMAKE_SOURCE_DIR}/third_party/protobuf/cmake/lib/cmake/protobuf/)
+      ${Protobuf_ROOT}/${CMAKE_INSTALL_LIBDIR}/cmake/protobuf/)
 
   message(STATUS "Setting up protobuf ...")
   execute_process(
     COMMAND
-      ${CMAKE_COMMAND} -D protobuf_BUILD_TESTS=OFF -D
-      protobuf_MSVC_STATIC_RUNTIME=OFF -D protobuf_BUILD_LIBPROTOC=ON -D
-      protobuf_BUILD_PROTOC_BINARIES=ON -D CMAKE_POSITION_INDEPENDENT_CODE=ON -G
-      "${CMAKE_GENERATOR}" .
+      ${CMAKE_COMMAND} -D protobuf_BUILD_TESTS=OFF
+        -D protobuf_MSVC_STATIC_RUNTIME=OFF
+        -D protobuf_VERBOSE=ON
+        -D protobuf_BUILD_LIBPROTOC=ON
+        -D protobuf_BUILD_PROTOC_BINARIES=ON
+        -D CMAKE_POSITION_INDEPENDENT_CODE=ON
+        -G "${CMAKE_GENERATOR}" .
     RESULT_VARIABLE result
     WORKING_DIRECTORY ${Protobuf_ROOT})
   if(result)
@@ -34,12 +37,12 @@ else()
     message(FATAL_ERROR "Failed to build protobuf (${result})!")
   endif()
 
-  find_package(Protobuf REQUIRED HINTS ${Protobuf_ROOT}/lib/cmake/protobuf)
+  find_package(Protobuf REQUIRED HINTS ${Protobuf_DIR})
 
-  include(${Protobuf_ROOT}/lib/cmake/protobuf/protobuf-config.cmake)
-  include(${Protobuf_ROOT}/lib/cmake/protobuf/protobuf-module.cmake)
-  include(${Protobuf_ROOT}/lib/cmake/protobuf/protobuf-options.cmake)
-  include(${Protobuf_ROOT}/lib/cmake/protobuf/protobuf-targets.cmake)
+  include(${Protobuf_DIR}/protobuf-config.cmake)
+  include(${Protobuf_DIR}/protobuf-module.cmake)
+  include(${Protobuf_DIR}/protobuf/protobuf-options.cmake)
+  include(${Protobuf_DIR}/protobuf/protobuf-targets.cmake)
 
   if(Protobuf_FOUND)
     message(STATUS "Protobuf version : ${Protobuf_VERSION}")
