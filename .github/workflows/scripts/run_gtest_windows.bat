8711d24b22fd01bf2cd39ad0d7ec1987db1ac89a
Cebere Bogdan <bogdan.cebere@gmail.com>
2021-01-20 20:17:32 +0200

    update windows workflows

--- .github/workflows/scripts/run_gtest_windows.bat
+++ .github/workflows/scripts/run_gtest_windows.bat
@@ -2,8 +2,8 @@ echo on
 
 set _CL_=/MT
 
-cmake -D BUILD_TEST=TRUE .
+cmake -D BUILD_TEST=TRUE -DCMAKE_BUILD_TYPE=Release .
 cmake -D BUILD_TEST=TRUE --build .
-cmake --build . --target install
+cmake --build . --target install --config Release
 
-.\Debug\tenseal_tests.exe
+.\Release\tenseal_tests.exe
