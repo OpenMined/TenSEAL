45b047b7d099c78b56a70de9fbd32d6dfde8694c
Cebere Bogdan <bogdan.cebere@gmail.com>
2021-02-01 21:19:00 +0200

    yum issue

--- .github/workflows/scripts/install_req_centos.sh
+++ .github/workflows/scripts/install_req_centos.sh
@@ -6,8 +6,8 @@ set -e
 curl https://github.com/Kitware/CMake/releases/download/v3.17.1/cmake-3.17.1-Linux-x86_64.tar.gz -L | tar xz -C /opt/
 echo "/opt/cmake-3.17.1-Linux-x86_64/bin" >> $GITHUB_PATH
 
-yum makecache
-yum install clang
+yum makecache -y
+yum install clang -y
 
 clang --version
 
