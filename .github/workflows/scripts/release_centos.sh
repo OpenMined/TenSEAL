7aa627a0db351b12adb610cda6fbc3def612ca5c
Cebere Bogdan <bogdan.cebere@gmail.com>
2021-02-01 22:15:21 +0200

    centos bugfixing

--- .github/workflows/scripts/release_centos.sh
+++ .github/workflows/scripts/release_centos.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-export CC=clang
-export CXX=clang++
+export PATH=/opt/rh/llvm-toolset-7.0/root/usr/bin:/opt/rh/llvm-toolset-7.0/root/usr/sbin:${PATH}
+export LD_LIBRARY_PATH=/opt/rh/llvm-toolset-7.0/root/usr/lib64:${LD_LIBRARY_PATH}
+
+export CC=/opt/rh/llvm-toolset-7.0/root/usr/bin/clang
+export CXX=/opt/rh/llvm-toolset-7.0/root/usr/bin/clang
 
 pip wheel . -w dist/ --no-deps
 auditwheel repair dist/*.whl --plat $AUDITWHEEL_PLAT
