a13f60e9a7c6f7f6a244b2e935837bd94dafe76f
Cebere Bogdan <bogdan.cebere@gmail.com>
2021-02-01 21:13:26 +0200

    debug release scripts

--- .github/workflows/scripts/install_req_centos.sh
+++ .github/workflows/scripts/install_req_centos.sh
@@ -7,8 +7,7 @@ curl https://github.com/Kitware/CMake/releases/download/v3.17.1/cmake-3.17.1-Lin
 echo "/opt/cmake-3.17.1-Linux-x86_64/bin" >> $GITHUB_PATH
 
 yum makecache
-yum install centos-release-scli centos-release-scl-rh llvm-toolset-7-clang llvm-toolset-7
-scl enable llvm-toolset-7 bash
+yum install clang
 
 clang --version
 
