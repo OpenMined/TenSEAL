5ac4f5ed4840a02b0ac4eb4a612bbba705231438
Cebere Bogdan <bogdan.cebere@gmail.com>
2021-02-01 21:06:03 +0200

    debug release script

--- .github/workflows/scripts/install_req_centos.sh
+++ .github/workflows/scripts/install_req_centos.sh
@@ -6,6 +6,13 @@ set -e
 curl https://github.com/Kitware/CMake/releases/download/v3.17.1/cmake-3.17.1-Linux-x86_64.tar.gz -L | tar xz -C /opt/
 echo "/opt/cmake-3.17.1-Linux-x86_64/bin" >> $GITHUB_PATH
 
+yum makecache
+yum install centos-release-scli centos-release-scl-rh llvm-toolset-7-clang llvm-toolset-7
+scl enable llvm-toolset-7 bash
+
+clang --version
+
+
 # install python dependencies
 python -m pip install --upgrade pip
 pip install setuptools wheel twine auditwheel
