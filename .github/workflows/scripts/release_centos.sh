321871b8035313e9448de09f811a7528b7240927
Cebere Bogdan <bogdan.cebere@gmail.com>
2021-02-02 07:11:26 +0200

    debug centos

--- .github/workflows/scripts/release_centos.sh
+++ .github/workflows/scripts/release_centos.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-export PATH=/opt/rh/llvm-toolset-7.0/root/usr/bin:/opt/rh/llvm-toolset-7.0/root/usr/sbin:${PATH}
-export LD_LIBRARY_PATH=/opt/rh/llvm-toolset-7.0/root/usr/lib64:${LD_LIBRARY_PATH}
-
 export CC=/opt/rh/llvm-toolset-7.0/root/usr/bin/clang
-export CXX=/opt/rh/llvm-toolset-7.0/root/usr/bin/clang
+export CPP=/opt/rh/llvm-toolset-7.0/root/usr/bin/clang-cpp
+export CXX=/opt/rh/llvm-toolset-7.0/root/usr/bin/clang++
+export PATH=/opt/rh/llvm-toolset-7.0/root/usr/bin:/opt/rh/llvm-toolset-7.0/root/usr/sbin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/opt/rh/llvm-toolset-7.0/root/usr/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 pip wheel . -w dist/ --no-deps
 auditwheel repair dist/*.whl --plat $AUDITWHEEL_PLAT
