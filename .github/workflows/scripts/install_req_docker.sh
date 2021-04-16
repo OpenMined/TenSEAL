f57e2864025458a0e814518fff321f2b51100680
Bogdan Cebere <bogdan.cebere@gmail.com>
2021-04-16 20:43:08 +0300

    Docker build

--- .github/workflows/scripts/install_req_docker.sh
+++ .github/workflows/scripts/install_req_docker.sh
@@ -3,11 +3,17 @@
 set -e
 
 apt update -y
-apt install curl git build-essential cmake automake libtool libtool-bin clang -y
+apt install wget curl git build-essential automake libtool libtool-bin clang -y
 
 export CC=clang
 export CXX=clang++
 
+cmake_install="cmake-3.17.0-Linux-x86_64.sh"
+wget https://github.com/Kitware/CMake/releases/download/v3.17.0/${cmake_install}
+sh ${cmake_install} --skip-license --prefix=/usr/local/ --exclude-subdir
+
 python -m pip install --upgrade pip
 pip install -r requirements_dev.txt
 pip install setuptools wheel twine auditwheel
+
+cmake --version
