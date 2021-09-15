0eb46a4a6d5a76832e2c2cf10fc3337168c6edaa
Bogdan Cebere <bogdan.cebere@gmail.com>
2021-09-15 21:50:54 +0100

    update SEAL

--- cmake/seal.cmake
+++ cmake/seal.cmake
@@ -7,7 +7,7 @@ set(SEAL_USE_INTEL_HEXL ON)
 FetchContent_Declare(
   com_microsoft_seal
   GIT_REPOSITORY https://github.com/microsoft/SEAL
-  GIT_TAG        1dd84a2066c1b12d56bbcec3e806a4d0653f2ca5 #v3.7
+  GIT_TAG        7ba34a956015efdd09321f6a4b3c7b09695c9bc4 #v3.7
 )
 FetchContent_MakeAvailable(com_microsoft_seal)
 
