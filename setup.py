a8b4bde1a642d7c718d056e9bc5c845591657665
Bogdan Cebere <bogdan.cebere@gmail.com>
2021-10-20 10:43:36 +0300

    add /p:TrackFileAccess=false to setup

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ class CMakeBuild(build_ext):
             cmake_args += [f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"]
             if sys.maxsize > 2 ** 32:
                 cmake_args += ["-A", "x64"]
-            build_args += ["--", "/m"]
+            build_args += ["--", "/m", "/p:TrackFileAccess=false"]
         else:
             cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
             build_args += ["--", "-j", "2"]
