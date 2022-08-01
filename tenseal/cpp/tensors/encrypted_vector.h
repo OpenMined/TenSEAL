110cd86e83fa837985460131f56af359ef7749b8
mrader1248 <mrader1248@gmail.com>
2022-08-01 09:21:12 +0200

    added comment

--- tenseal/cpp/tensors/encrypted_vector.h
+++ tenseal/cpp/tensors/encrypted_vector.h
@@ -186,6 +186,9 @@ class EncryptedVector : public EncryptedTensor<plain_t, encrypted_t> {
                 auto diag = matrix.get_diagonal(
                     -local_i,
                     this->tenseal_context()->template slot_count<encoder_t>());
+                
+                // don't add zero diagonals to (a) improve performance and (b)
+                // avoid transparent ciphertext issues
                 bool is_diag_nonzero = std::any_of(
                     diag.begin(), diag.end(), [](plain_t x) { return x != 0; });
                 if (is_diag_nonzero) {
