c0da4649c67f14705269a67ff0da7afc6446bd28
Bogdan Cebere <bogdan.cebere@gmail.com>
2023-01-04 15:25:37 +0200

    update gtest

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -2,7 +2,7 @@ if(${BUILD_TEST})
   FetchContent_Declare(
       googletest
       GIT_REPOSITORY https://github.com/google/googletest.git
-      GIT_TAG        release-1.10.0
+      GIT_TAG        release-1.12.1
   )
   FetchContent_MakeAvailable(googletest)
 
