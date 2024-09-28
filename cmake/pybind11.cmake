686df4b87ad3bc2200d5c522f324d4526a97e548
Bilal Retiat <bilal.retiat@gmail.com>
2024-09-28 21:38:48 +0400

    Use pybind11 v2.12.0 instead of v2.13.6

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   com_pybind_pybind11
   GIT_REPOSITORY https://github.com/pybind/pybind11
-  GIT_TAG        v2.13.6
+  GIT_TAG        v2.12.0
 )
 FetchContent_MakeAvailable(com_pybind_pybind11)
 
